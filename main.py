55c77ea3ac3ecc701fb666bbca726f6daf39b817
joelcrawford <joelrootcrawford@gmail.com>
2022-11-28 16:36:12 -0700

    starting the pseudo code

---
+++ main.py
@@ -0,0 +1,43 @@
+'''
+Input Text Processing
+
+
+'''
+
+# are we dropbox or event based processing?
+
+
+# dropbox ------------------------------------------
+
+# connect dropbox
+
+# file
+
+# path
+# filename
+
+# event --------------------------------------------
+
+# monitor github
+
+# file
+
+
+# both ---------------------------------------------------
+# characters
+# lines
+# folios
+# markup
+
+
+
+# send to database ---------------------------------------
+
+# match
+# control fields
+
+# collation
+
+
+
+
